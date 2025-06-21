5d66e2bef2a8f119d5ec8899e6087636afd10a17
tne-dev <tnerad.dev@outlook.com>
2025-06-21 16:55:24 +0200

    added devise for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!, unless: :devise_controller?
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+
+  def configure_permitted_parameters
+    # list all your extra fields here
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_surname])
+  end
 end

a07a5aeff8d3593aa6171e79eb71fdc0e7c3ce42
tne-dev <tnerad.dev@outlook.com>
2025-06-22 11:34:22 +0200

    add bullet to test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,29 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Monitor N+1 queries using bullet
+  config.after_initialize do
+    Bullet.enable = true
+    # Bullet.sentry = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    # Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
+    #                 :password => 'bullets_password_for_jabber',
+    #                 :receiver => 'your_account@jabber.org',
+    #                 :show_online_status => true }
+    Bullet.rails_logger = true
+    # Bullet.honeybadger = true
+    # Bullet.bugsnag = true
+    # Bullet.appsignal = true
+    # Bullet.airbrake = true
+    # Bullet.rollbar = true
+    # Bullet.add_footer = true
+    # Bullet.skip_html_injection = false
+    # Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
+    # Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware', ['my_file.rb', 'my_method'], ['my_file.rb', 16..20] ]
+    # Bullet.slack = { webhook_url: 'http://some.slack.url', channel: '#default', username: 'notifier' }
+    # Bullet.opentelemetry = true
+  end
 end
